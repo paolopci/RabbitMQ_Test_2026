2ddeae82bfff5032e6f5f94d8dbdc4e7add44150
Paolo <paolo@example.com>
2026-09-08 11:03:53 +0200

    complete AnalyticsConsumer project

--- 4-Routing/AnalyticsConsumer/Program.cs
+++ 4-Routing/AnalyticsConsumer/Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+
+
+
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "routing",
+    type: ExchangeType.Direct
+);
+
+var queueName = (await channel.QueueDeclareAsync()).QueueName;
+
+await channel.QueueBindAsync(
+    queue: queueName,
+    exchange: "routing",
+    routingKey: "analyticsonly"
+);
+
+await channel.QueueBindAsync(
+    queue: queueName,
+    exchange: "routing",
+    routingKey: "both"
+);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Analitics - Recieved new Message: {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(
+    queue: queueName,   // Coda da cui ricevere i messaggi.
+    autoAck: true,      // RabbitMQ considera il messaggio confermato alla consegna.
+    consumer: consumer // Oggetto che riceve i messaggi tramite ReceivedAsync.
+);
+
+Console.WriteLine("Analytics Consuming");
+Console.ReadKey();
--- 4-Routing/Producer/Program.cs
+++ 4-Routing/Producer/Program.cs
@@ -1,7 +1,10 @@
-﻿
-
-using System.Text;
+﻿using System.Text;
 using RabbitMQ.Client;
+
+
+
+
+
 
 var factory = new ConnectionFactory() { HostName = "localhost" };
 using var connection = await factory.CreateConnectionAsync();
