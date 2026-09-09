6af15bdf13b1dd05a4e9b5d8425f841f21f37611
Paolo <paolo@example.com>
2026-09-09 11:17:26 +0200

    complete Producer project

--- 5-Topics/Producer/Program.cs
+++ 5-Topics/Producer/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+
+
+
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "topic",
+    type: ExchangeType.Topic // instradamento tramite patter routing key
+);
+
+var userPaymentsMessage = "A european user paid for something";
+var userPaymentsBody = Encoding.UTF8.GetBytes(userPaymentsMessage);
+await channel.BasicPublishAsync(
+    exchange: "topic",
+    routingKey: "user.europe.payments",
+    body: userPaymentsBody
+);
+
+Console.WriteLine($"Send message: {userPaymentsMessage}");
+
+var businessOrderMessage = "A european business ordered goods";
+
+var businessOrderBody = Encoding.UTF8.GetBytes(businessOrderMessage);
+
+await channel.BasicPublishAsync(exchange: "topic", routingKey: "business.europe.order", businessOrderBody);
+
+Console.WriteLine($"Send message: {businessOrderMessage}");
